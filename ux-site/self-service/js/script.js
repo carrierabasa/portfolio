976794d889955ee0ff86c3f1d9faf7678fccd7f3
carrierabasa <carrie.knittel@gmail.com>
2019-01-23 07:48:02 -0800

    old ux site
    adding files

---
+++ ux-site/self-service/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $('.table-fixed-header').fixedHeader();
+});
+
+//  $(window).on('resize', function () {
+//    $('.header-copy').width($('.table-fixed-header').width())
+//});
